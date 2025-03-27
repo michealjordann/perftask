9e20ef06f727dbaf01ed1b03c6fba3220b216cf3
Tanush P <148394954+michealjordann@users.noreply.github.com>
2025-03-27 14:32:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ def reactest():
         begin = input("Type in go to start.")
         if begin == "go":
             print("As soon as you see the letter X appear in the terminal, press Enter.")
-            time.sleep(random.randint(1,5))
-            letter = input("x")
+            wait = time.sleep(random.randint(2,6))
             start = time.time()
+            letter = input("x")
             end = time.time()
-            if letter == "":
-                print("Nice! Your reaction time is ", start - end)
+            if 
+                print("Nice! Your reaction time is", end-start)
             else:
                 print("Do not type anything but enter! Try again.")
+                break
         elif begin != "go":
             print("wrong input to start")
             
