5d35ac58d890da0ebbbfc32a43ade5a3428ba432
Tanush P <148394954+michealjordann@users.noreply.github.com>
2025-04-01 15:17:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import time
 import random
 import tkinter as tk
-#GUI
-r = tk.Tk()
-r.title("Reaction Time Test By Tanush Pusulrui")
-r.geometry("400x300")
+#leaderboard setup
+def ldbsetup():
+    user = input("type in your name to save your time to the leaderboard.")
+    names.append(user)
+
+def ldb():
+    print("add a way to show the best time here with rtime idk how yet")
 #main game
 going = True
 scores = []
+names = []
 def reactest():
     while going == True:
         #begin test
-        begin = input("Type in go to start.")
+        begin = input("Type in go to start, or type in 1 for leaderboard.")
         if begin == "go":
+            ldbsetup()
             print("As soon as you see the letter X appear in the terminal, press Enter.")
             time.sleep(random.randint(2,6))
             start = time.time()
@@ -21,14 +26,20 @@ def reactest():
             rtime = round(end - start,3)
             if rtime <= 0.101:
                 print("you pressed enter too early! try again")
-                break
+                continue
             if letter == "":
                 print("Nice! Your reaction time is", rtime)
+                scores.append(rtime)
             else:
                 print("Do not type anything but enter! Try again.")
-                break
-        elif begin != "go":
+                continue
+        #leaderboard
+        elif begin == "1":
+            print("pravin rasiah")
+        else:
             print("wrong input to start")
+            continue
             
-
 reactest()
+
+
